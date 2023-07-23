ec949ade0c0718c463e93cb50a9f7cd7daef2ca9
Rhuan Henrique <rhuanhenrique964@gmail.com>
2023-07-22 22:58:07 -0300

    Ajustando a alteração do idioma: armazenando essa informação no localstorage

--- public/script.js
+++ public/script.js
@@ -1,3 +1,36 @@
+let portuguese = 'portuguese'
+let english = 'english'
+
+//Português
+let h1Portuguese = 'Olá';
+let h2Portuguese = 'Meu nome é';
+let h3Portuguese = 'e eu sou um dev fullstack :)';
+
+let sobreBtnPortugues = 'sobre mim';
+let projetosBtnPortugues = 'meus projetos';
+let contatoBtnPortugues = 'contato';
+
+//Inglês
+let h1English = 'Hi';
+let h2English = 'My name is';
+let h3English = "and i'm a fullstack developer.";
+
+let sobreBtnEnglish = 'about me';
+let projetosBtnEnglish = 'my projects';
+let contatoBtnEnglish = 'contact';
+
+if(localStorage.getItem('language')){
+    let languageSite = localStorage.getItem('language');
+    
+    if(languageSite == 'portuguese'){
+        changeForPortuguese();
+    }
+    
+    if(languageSite == 'portuguese'){
+        changeForPortuguese();
+    }
+}
+
 let languages_list = document.querySelector('.languages_list');
 
 let languages_list_li_qtds = (languages_list.childNodes.length - 1) / 2;
@@ -9,17 +42,11 @@ languages_list.addEventListener('click', ()=>{
 
 
 //Mudar o idioma da página
-let h1Portuguese = 'Olá';
-let h2Portuguese = 'Meu nome é';
-let h3Portuguese = 'e eu sou um dev fullstack :)';
-
-let sobreBtnPortugues = 'sobre mim';
-let projetosBtnPortugues = 'meus projetos';
-let contatoBtnPortugues = 'contato';
 
 document.querySelector('.portuguese').addEventListener('click', changeForPortuguese);
 
 function changeForPortuguese(){
+    language = localStorage.setItem('language', portuguese);
     document.querySelector('h1').innerHTML = h1Portuguese;
     document.querySelector('.apresentationH2').innerHTML = h2Portuguese;
     document.querySelector('h3').innerHTML = h3Portuguese;
@@ -27,19 +54,20 @@ function changeForPortuguese(){
     document.querySelector('#aboutMe_btn').innerHTML = sobreBtnPortugues;
     document.querySelector('#myProjects_btn').innerHTML = projetosBtnPortugues;
     document.querySelector('#contacts').innerHTML = contatoBtnPortugues;
+    document.querySelector('.contact__title').innerHTML = contatoBtnPortugues.replace('c', 'C');
+    document.querySelector('#nome').placeholder = 'Seu nome';
+    document.querySelector('#email').placeholder = 'Seu e-mail';
+    document.querySelector('#text__area').placeholder = 'Mensagem';
+    document.querySelector('.btnEnviar').innerHTML = 'Enviar';
+    if(document.querySelector('.campo__erro_contato')){
+        document.querySelector('.campo__erro_contato').innerHTML = 'Preencha todos os campos';
+    }
 };
-
-let h1English = 'Hi';
-let h2English = 'My name is';
-let h3English = "and i'm a fullstack developer.";
-
-let sobreBtnEnglish = 'about me';
-let projetosBtnEnglish = 'my projects';
-let contatoBtnEnglish = 'contact';
 
 document.querySelector('.english').addEventListener('click', changeForEnglish);
 
 function changeForEnglish(){
+    language = localStorage.setItem('language', english);
     document.querySelector('h1').innerHTML = h1English;
     document.querySelector('.apresentationH2').innerHTML = h2English;
     document.querySelector('h3').innerHTML = h3English;
@@ -47,6 +75,14 @@ function changeForEnglish(){
     document.querySelector('#aboutMe_btn').innerHTML = sobreBtnEnglish;
     document.querySelector('#myProjects_btn').innerHTML = projetosBtnEnglish;
     document.querySelector('#contacts').innerHTML = contatoBtnEnglish;
+    document.querySelector('.contact__title').innerHTML = contatoBtnEnglish.replace('c', 'C');
+    document.querySelector('#nome').placeholder = 'Your name';
+    document.querySelector('#email').placeholder = 'Yout e-mail';
+    document.querySelector('#text__area').placeholder = 'Message';
+    document.querySelector('.btnEnviar').innerHTML = 'Send';
+    if(document.querySelector('.campo__erro_contato')){
+        document.querySelector('.campo__erro_contato').innerHTML = 'Fill in all fields';
+    }
 };
 
 
