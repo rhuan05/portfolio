6612b5381f7507ef474e46dbe4d7f8f5e679885c
Rhuan Henrique <rhuanhenrique964@gmail.com>
2023-07-28 16:44:52 -0300

    Ajustando a aba "meus projetos" para celular: Adicionando o scroll nos projetos.

--- public/script.js
+++ public/script.js
@@ -26,8 +26,8 @@ if(localStorage.getItem('language')){
         changeForPortuguese();
     }
     
-    if(languageSite == 'portuguese'){
-        changeForPortuguese();
+    if(languageSite == 'english'){
+        changeForEnglish();
     }
 }
 
@@ -62,17 +62,17 @@ function changeForPortuguese(){
     if(document.querySelector('.campo__erro_contato')){
         document.querySelector('.campo__erro_contato').innerHTML = 'Preencha todos os campos';
     }
-    document.querySelector('#h1_ebook_project').innerHTML = 'Página de vendas para lançamento';
     document.querySelector('#p1_about_ebook_project').innerHTML = 'Essa página foi criada com o objetivo de promover um e-book. Onde aconteceria uma campanha, rodando tráfego pago no Facebook ADS apontando para essa página.';
     document.querySelector('#p2_tech_ebook_project').innerHTML = 'Tecnologias usadas: HTML, CSS e JavaScript';
     
-    document.querySelector('#h1_eccomerce_project').innerHTML = 'E-commerce com paínel de admin';
     document.querySelector('#p1_about_ecomerce_project').innerHTML = 'Um e-commerce onde o proprietário da loja (jf-construções) consegue criar promoções, produtos e adicionar novidades da loja pelo banner principal.';
     document.querySelector('#p2_tech_ecommerce_project').innerHTML = 'Tecnologias usadas: HTML, CSS, JavaScript, NodeJS, MongoDB e Cloudinary (que não é uma tecnologia mas foi usado para o armazenamento de imagens na nuvem).';
     
-    document.querySelector('#h1_chat_project').innerHTML = 'Chat interativo';
     document.querySelector('#p1_about_chat_project').innerHTML = 'Um chat (igual ao whatsapp mesmo!) que facilita a conversa em tempo real entre duas ou mais pessoas.';
     document.querySelector('#p2_tech_chat_project').innerHTML = 'Tecnologias usadas: HTML, CSS, JavaScript, NodeJS e Socket.io';
+    
+    document.querySelector('.myProjects__title').innerHTML = 'Meus projetos';
+    document.querySelector('.sobremim__title').innerHTML = 'Sobre mim';
 };
 
 document.querySelector('.english').addEventListener('click', changeForEnglish);
@@ -94,17 +94,17 @@ function changeForEnglish(){
     if(document.querySelector('.campo__erro_contato')){
         document.querySelector('.campo__erro_contato').innerHTML = 'Fill in all fields';
     }
-    document.querySelector('#h1_ebook_project').innerHTML = 'Launch sales page';
     document.querySelector('#p1_about_ebook_project').innerHTML = 'This page was created with the aim of promoting an e-book. Where a campaign would take place, running paid traffic on Facebook ADS pointing to that page.';
     document.querySelector('#p2_tech_ebook_project').innerHTML = 'Technologies used: HTML, CSS and JavaScript';
     
-    document.querySelector('#h1_eccomerce_project').innerHTML = 'E-commerce with admin panel';
     document.querySelector('#p1_about_ecomerce_project').innerHTML = 'An e-commerce where the store owner (jf-construções) can create promotions, products and add store news through the main banner.';
     document.querySelector('#p2_tech_ecommerce_project').innerHTML = 'Technologies used: HTML, CSS, JavaScript, NodeJS, MongoDB and Cloudinary (which is not a technology but was used for storing images in the cloud).';
     
-    document.querySelector('#h1_chat_project').innerHTML = 'Interactive chat';
     document.querySelector('#p1_about_chat_project').innerHTML = 'A chat (just like whatsapp!) that facilitates real-time conversation between two or more people.';
     document.querySelector('#p2_tech_chat_project').innerHTML = 'Technologies used: HTML, CSS, JavaScript, NodeJS and Socket.io';
+
+    document.querySelector('.sobremim__title').innerHTML = 'About me';
+    document.querySelector('.myProjects__title').innerHTML = 'My projects';
 };
 
 
@@ -170,4 +170,31 @@ if(main__contact.style.top == '0vh'){
 document.querySelector('.x_iconcontact').addEventListener('click', ()=>{
     document.querySelector('.main__contact').style.top = '100vh'
     document.querySelector('.contact__title').style.transform = 'translateX(-200px)'
+});
+
+//Scroll in phone
+let arrow_right = document.querySelector('#arrow_right');
+let sroll_for_phone = document.querySelector('.sroll_for_phone');
+let marginDefault = 0;
+
+arrow_right.addEventListener('click', ()=>{
+    if(marginDefault !== -200){
+        marginDefault = marginDefault - 100;
+        sroll_for_phone.style.marginLeft = `${marginDefault}%`;
+    }else{
+        marginDefault = 0;
+        sroll_for_phone.style.marginLeft = `${marginDefault}%`;
+    }
+});
+
+let arrow_left = document.querySelector('#arrow_left');
+
+arrow_left.addEventListener('click', ()=>{
+    if(marginDefault !== 0){
+        marginDefault = marginDefault + 100;
+        sroll_for_phone.style.marginLeft = `${marginDefault}%`;
+    }else{
+        marginDefault = 0;
+        sroll_for_phone.style.marginLeft = `${marginDefault}%`;
+    }
 });
