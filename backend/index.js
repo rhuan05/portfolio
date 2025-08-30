b22aea3585963f40ef2222a9a3d79b29e8179ce5
rhuan05 <rhuanhenrique964@gmail.com>
2025-08-30 15:24:37 -0300

    Add npm start in package.json

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ const app = express();
 const routes = require('./routes/routes');
 const path = require('path');
 
+app.set('views', path.join(__dirname, '../frontend/views'));
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/views'));
 
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
